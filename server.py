a2dcbf73e0b5ceef210f1e8f4e18ee063e5182ee
Andrei K <1____1@bk.ru>
2020-02-23 17:06:05 +0300

    add bus recv websocket

--- server.py
+++ server.py
@@ -1,23 +1,55 @@
+import json
 import trio
+
 from trio_websocket import serve_websocket, ConnectionClosed
+
+
+def get_bus_from_file(filename):
+    with open(filename, 'r') as f:
+        bus = json.loads(f.read())
+    bus_name = bus.get('name')
+    bus_coordinates = bus.get('coordinates')
+    return bus_name, bus_coordinates
+
+
+def make_message(route, coordinate):
+    message = {
+        "msgType": "Buses",
+        "buses": [
+            {"busId": "c790сс", "lat": coordinate[0], 
+             "lng": coordinate[1], "route": route},
+        ],
+    }
+    return message
+
 
 async def echo_server(request):
     ws = await request.accept()
+    route, coordinates = get_bus_from_file('156.json')
+
     while True:
         try:
-            #message = await ws.get_message()
-            message = {"msgType": "Buses",
-                        "buses": [
-                            {"busId": "c790сс", "lat": 55.7500, "lng": 37.600, "route": "120"},
-    {"busId": "a134aa", "lat": 55.7494, "lng": 37.621, "route": "670к"},
-  ]
-}
-            await ws.send_message(message)
+            for coordinate in coordinates:
+                message = make_message(route, coordinate)
+                await trio.sleep(1)
+                await ws.send_message(json.dumps(message))
+        except ConnectionClosed:
+            break
+
+
+async def server(request):
+    ws = await request.accept()
+    while True:
+        try:
+            message = await ws.get_message()
+            print(message)
+            #await ws.send_message(message)
         except ConnectionClosed:
             break
 
 async def main():
-    await serve_websocket(echo_server, '127.0.0.1', 8000, ssl_context=None)
+    #await serve_websocket(echo_server, "127.0.0.1", 8000, ssl_context=None)
+    await serve_websocket(server, "127.0.0.1", 8080, ssl_context=None)
 
 if __name__ == "__main__":
     trio.run(main)
