4bbe040fc38be040a68c700437d4c244a1b25d1d
Andrei K <1____1@bk.ru>
2020-02-24 16:09:02 +0300

    add multiple coros with bus routes

--- fake_bus.py
+++ fake_bus.py
@@ -4,6 +4,16 @@ import json
 from sys import stderr
 from trio_websocket import open_websocket_url
 
+import os
+
+
+def load_routes(directory_path='routes'):
+    for filename in os.listdir(directory_path):
+        if filename.endswith(".json"):
+            filepath = os.path.join(directory_path, filename)
+            with open(filepath, 'r', encoding='utf8') as file:
+                yield json.load(file)
+
 
 def get_bus_from_file(filename):
     with open(filename, 'r') as f:
@@ -19,15 +29,23 @@ def make_message(route, coordinate):
     return message
 
 
+async def run_bus(url, bus_id, route):
+    async with open_websocket_url(url) as ws:
+        while True:
+            for coordinate in route:
+                message = make_message(bus_id, coordinate)
+                await ws.send_message(json.dumps(message))
+                await trio.sleep(1)
+
+
 async def main():
-    bus, coordinates = get_bus_from_file("156.json")
     try:
-        async with open_websocket_url('ws://127.0.0.1:8080') as ws:
-            while True:
-                for coordinate in coordinates:
-                    message = make_message(bus, coordinate)
-                    await ws.send_message(json.dumps(message))
-                    await trio.sleep(1)
+        url = 'ws://127.0.0.1:8080'
+        async with trio.open_nursery() as nursery:
+            for route in load_routes():
+                bus = route.get('name')
+                coordinates = route.get('coordinates')
+                nursery.start_soon(run_bus,url, bus, coordinates)
     except OSError as ose:
         print('Connection attempt failed: %s' % ose, file=stderr)
 
