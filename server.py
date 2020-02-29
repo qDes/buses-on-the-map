6049851e7b24ff2bf3f123ae8922762b4541cde3
Andrei K <1____1@bk.ru>
2020-02-29 12:50:45 +0300

    add buses info writer to browser ws

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
 import json
 import trio
 
+from functools import partial
 from trio_websocket import serve_websocket, ConnectionClosed
+
+buses = dict()
 
 
 def get_bus_from_file(filename):
@@ -23,33 +26,57 @@ def make_message(route, coordinate):
     return message
 
 
-async def echo_server(request):
+def make_message_to_browser(buses):
+    message = {
+        "msgType": "Buses",
+        "buses": [],
+    }
+    for bus_id, bus_info in buses.items():
+        bus_info = bus_info.get("busInfo")
+        bus = {"busId": bus_id, "lat": bus_info.get('lat'), 
+                "lng": bus_info.get("lng"), "route": bus_info.get("route")}
+        message["buses"].append(bus)
+    return message
+ 
+
+async def talk_to_browser(request):
+    global buses
     ws = await request.accept()
     route, coordinates = get_bus_from_file('156.json')
-
     while True:
         try:
-            for coordinate in coordinates:
-                message = make_message(route, coordinate)
-                await trio.sleep(1)
-                await ws.send_message(json.dumps(message))
+            message = make_message_to_browser(buses)
+            print(message)
+            await ws.send_message(json.dumps(message))
+            await trio.sleep(1)
         except ConnectionClosed:
             break
 
 
-async def server(request):
+async def get_buses(request):
+    global buses
     ws = await request.accept()
     while True:
         try:
             message = await ws.get_message()
-            print(message)
-            #await ws.send_message(message)
+            message = json.loads(message)
+            buses[message.get("busId")] = {"busInfo":{"lat":message.get("lat"),
+        "lng": message.get("lng"), "route": message.get("route")}}
         except ConnectionClosed:
             break
 
+
 async def main():
-    #await serve_websocket(echo_server, "127.0.0.1", 8000, ssl_context=None)
-    await serve_websocket(server, "127.0.0.1", 8080, ssl_context=None)
+    bus_reader = partial(serve_websocket, get_buses,
+                         "127.0.0.1", 8080, ssl_context=None)
+    bus_sender = partial(serve_websocket, talk_to_browser,
+                         "127.0.0.1", 8000, ssl_context=None)
+    async with trio.open_nursery() as nursery:
+    #await serve_websocket(talk_to_browser, "127.0.0.1", 8000, ssl_context=None)
+    #await serve_websocket(test) #get_buses, "127.0.0.1", 8080, ssl_context=None)
+        #server = await nursery.start_soon(serve_websocket,server, "127.0.0.1", 8080, ssl_context=None)
+        nursery.start_soon(bus_reader)
+        nursery.start_soon(bus_sender)
 
 if __name__ == "__main__":
     trio.run(main)
