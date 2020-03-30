f7de3d8fea7032b7f9c0b56465f33e73714b5a98
Andrei K <1____1@bk.ru>
2020-03-31 01:00:02 +0300

    add memory channels for fake_bus

--- fake_bus.py
+++ fake_bus.py
@@ -34,7 +34,18 @@ def generate_bus_id(route_id, bus_index):
     return f"{route_id}-{bus_index}"
 
 
-async def run_bus(url, bus_id, route):
+async def run_bus(send_chanel, bus_id, route):
+    start_offset = random.randint(1, len(route))
+    for coordinate in route[start_offset:]:
+        message = make_message(bus_id, coordinate)
+        await send_chanel.send(json.dumps(message, ensure_ascii=False))
+        await trio.sleep(1)
+    while True:
+        for coordinate in route:
+            message = make_message(bus_id, coordinate)
+            await send_chanel.send(json.dumps(message, ensure_ascii=False))
+            await trio.sleep(1)
+    '''   
     async with open_websocket_url(url) as ws:
         start_offset = random.randint(1, len(route))
         for coordinate in route[start_offset:]:
@@ -48,17 +59,46 @@ async def run_bus(url, bus_id, route):
                 await ws.send_message(json.dumps(message,
                                                  ensure_ascii=False))
                 await trio.sleep(1)
+    '''
 
+
+async def send_updates(server_address, receive_channel):
+    async with open_websocket_url(server_address) as ws:
+        while True:
+            pass
+            message = await receive_channel.receive()
+            await ws.send_message(message)
+'''
+async def producer(send_channel):
+    # Producer sends 3 messages
+    for i in range(3):
+        # The producer sends using 'await send_channel.send(...)'
+        await send_channel.send("message {}".format(i))
+
+
+async def consumer(receive_channel):
+    # The consumer uses an 'async for' loop to receive the values:
+    async for value in receive_channel:
+        print("got value {!r}".format(value))
+'''
 
 async def main():
     try:
         url = 'ws://127.0.0.1:8080'
         async with trio.open_nursery() as nursery:
+            # Open a channel:
+            send_channel, receive_channel = trio.open_memory_channel(0)
+            # Start a producer and a consumer, passing one end of the channel to
+            # each of them:
+            # nursery.start_soon(producer, send_channel)
+            # nursery.start_soon(consumer, receive_channel)
+            
             for route in load_routes():
-                for i in range(5):
+                for i in range(15):
                     bus = generate_bus_id(route.get('name'), i)
                     coordinates = route.get('coordinates')
-                    nursery.start_soon(run_bus, url, bus, coordinates)
+                    nursery.start_soon(run_bus, send_channel, bus, coordinates)
+            nursery.start_soon(send_updates, url, receive_channel)
     except OSError as ose:
         print('Connection attempt failed: %s' % ose, file=stderr)
 
