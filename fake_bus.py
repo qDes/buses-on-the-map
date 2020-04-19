0bdf0ad362a74c7cf08dc795e2c325826ebac5a3
Andrei K <1____1@bk.ru>
2020-04-19 22:39:18 +0300

    add cli for faker

--- fake_bus.py
+++ fake_bus.py
@@ -1,13 +1,16 @@
-import trio
 import json
+import logging
 import os
 import random
+import trio
 
+import asyncclick as click
 from sys import stderr
 from trio_websocket import open_websocket_url
+from itertools import islice
 
 
-def load_routes(directory_path='routes'):
+def load_routes(directory_path):
     for filename in os.listdir(directory_path):
         if filename.endswith(".json"):
             filepath = os.path.join(directory_path, filename)
@@ -25,12 +28,12 @@ def get_bus_from_file(filename):
 
 def make_message(route, coordinate):
     message = {"busId": f"{route}-0", "lat": coordinate[0],
-             "lng": coordinate[1], "route": route}
+               "lng": coordinate[1], "route": route}
     return message
 
 
-def generate_bus_id(route_id, bus_index):
-    return f"{route_id}-{bus_index}"
+def generate_bus_id(route_id, bus_index, emulator_id):
+    return f"{route_id}-{bus_index}-{emulator_id}"
 
 
 async def run_bus(send_chanel, bus_id, route):
@@ -49,32 +52,49 @@ async def run_bus(send_chanel, bus_id, route):
 async def send_updates(server_address, receive_channel):
     async with open_websocket_url(server_address) as ws:
         while True:
-            pass
             message = await receive_channel.receive()
             await ws.send_message(message)
 
 
-async def main():
+@click.command()
+@click.option("-s", "--server", default="127.0.0.1:8080")
+@click.option("-r", "--routes_number", default=50)
+@click.option("-b", "--buses_per_route", default=5)
+@click.option("-w", "--websockets_number", default=3)
+@click.option("-e", "--emulator_id", default=0)
+@click.option("-t", "--refresh_timeout")
+@click.option("-v", default=False, help="enable logging")
+async def main(server, routes_number, buses_per_route,
+               websockets_number, emulator_id,
+               refresh_timeout, v):
+    if v:
+        FORMAT = "%(levelname)s:root: %(message)s"
+        logging.basicConfig(format=FORMAT, level=logging.DEBUG)
     try:
-        url = 'ws://127.0.0.1:8080'
+        url = f"ws://{server}"
         async with trio.open_nursery() as nursery:
             # Open a channel:
             send_channels = []
             receive_channels = []
-            for _ in range(3):
+            for _ in range(websockets_number):
                 send_channel, receive_channel = trio.open_memory_channel(0)
                 send_channels.append(send_channel)
                 receive_channels.append(receive_channel)
-            for route in load_routes():
-                for i in range(15):
-                    bus = generate_bus_id(route.get('name'), i)
+            for route in islice(load_routes("routes"), 0, routes_number):
+                for bus_num in range(buses_per_route):
+                    bus = generate_bus_id(route.get('name'),
+                                          bus_num,
+                                          emulator_id)
                     coordinates = route.get('coordinates')
-                    nursery.start_soon(run_bus, random.choice(send_channels), bus, coordinates)
+                    nursery.start_soon(run_bus,
+                                       random.choice(send_channels),
+                                       bus,
+                                       coordinates)
             for receive_channel in receive_channels:
                 nursery.start_soon(send_updates, url, receive_channel)
     except OSError as ose:
-        print('Connection attempt failed: %s' % ose, file=stderr)
+        logging('Connection attempt failed: %s' % ose, file=stderr)
 
 
 if __name__ == "__main__":
-    trio.run(main)
+    main(_anyio_backend="trio")
